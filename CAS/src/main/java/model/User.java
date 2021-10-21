3cf6e833eb6147e43547e48e725e3a00d4fc5deb
wkf-87 <77309729+wkf-87@users.noreply.github.com>
2021-10-21 16:14:35 +0800

    通过http协议与子系统交互

---
+++ CAS/src/main/java/model/User.java
@@ -0,0 +1,51 @@
+package model;
+
+public class User {
+
+    private Integer id;
+    private String name;
+    private Integer sex;
+    private Integer age;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private  String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
